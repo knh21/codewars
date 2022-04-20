9fdd04288bd6fe374aeca383ed368a0f5730c8fa
Kyle Hill <kyle.hill56@yahoo.com>
2022-04-19 20:28:42 -0400

    convert a boolean to the string version

---
+++ booleantostring8kyu/index.js
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+
+function booleanToString(b){
+    let newStr = b.toString()
+    return newStr
+  }
