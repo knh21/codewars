c4f83c24e826e831fbd95e4beedcb9e8b8bc7786
Kyle Hill <kyle.hill56@yahoo.com>
2022-05-16 20:59:24 -0400

    given an integer find the opposite

---
+++ oppositenum/index.js
@@ -0,0 +1,19 @@
+// given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// P- going to be a whole number or decimal number
+// R- return a number
+// E- console.log( opposite(1))=> output -1
+// P- have to get an opposite number from the given number, have to subtract something from number to get a negative number and vice versa, the only number that would stay constant is 0
+
+
+function opposite(number) {
+    //your code here
+    let opNum = 0 - number
+    return opNum
+}
