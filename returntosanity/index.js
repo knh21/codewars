ff9a8e3e8ba2a23b14277c1a97cd7bcaa20acc8c
Kyle Hill <kyle.hill56@yahoo.com>
2022-07-01 11:14:06 -0400

    bug kata

---
+++ returntosanity/index.js
@@ -0,0 +1,14 @@
+//This function should return an object, but it's not doing what's intended. What's wrong?
+// Given function
+// function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return results;
+//   }
+
+// corrected function
+function mystery() {
+    let results =
+    {sanity: 'Hello'}
+    return results
+}
