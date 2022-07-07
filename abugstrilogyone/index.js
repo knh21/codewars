fe9e592ef8f0be30096aad98308eb2b10d70f58c
Kyle Hill <kyle.hill56@yahoo.com>
2022-07-06 20:56:29 -0400

    bugs kata

---
+++ abugstrilogyone/index.js
@@ -0,0 +1,17 @@
+// Our friend Pac has finally decided to pursue a career in the coding industry.
+// He is a newbie, he needs to learn properly.
+// Therefore, Pac wants to apply for the worldwide famous -and very demanding-
+// 'C0d3r 1ns1d3' Academy for beginners.
+// In order to join, Pac is required to solve a series of 3 exercises about 'Bug Fixes'.
+
+
+function yourFutureCareer() {
+	let career = Math.random()
+	if (career <= 0.32) {
+		return 'FrontEnd Developer'
+    }else if (career <= 0.65){
+		return 'BackEnd Developer'
+	} else {
+		return 'Full-Stack Developer'
+	}
+}
